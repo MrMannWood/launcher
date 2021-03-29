cb7ce2b29f1246f653081d272c861edf03fe54b3
Marshall Mann-Wood <marshallmannwood@gmail.com>
2021-03-28 18:22:35 -0700

    removed unused imports

--- app/src/main/java/com/mrmannwood/hexlauncher/launcher/LauncherActivity.kt
+++ app/src/main/java/com/mrmannwood/hexlauncher/launcher/LauncherActivity.kt
@@ -5,11 +5,8 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.view.WindowCompat
 import com.mrmannwood.hexlauncher.HandleBackPressed
 import com.mrmannwood.hexlauncher.applist.AppListFragment
 import com.mrmannwood.launcher.R

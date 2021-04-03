94c66c0e8f18273119cc68dbf8bf93766d2fb5f7
Marshall Mann-Wood <marshallmannwood@gmail.com>
2021-04-03 10:28:52 -0700

    first pass of lint

--- app/src/main/java/com/mrmannwood/hexlauncher/launcher/AppInfo.kt
+++ app/src/main/java/com/mrmannwood/hexlauncher/launcher/AppInfo.kt
@@ -1,6 +1,7 @@
 package com.mrmannwood.hexlauncher.launcher
 
 import android.graphics.drawable.Drawable
+import java.util.*
 
 data class AppInfo(
     val packageName: String,
@@ -8,6 +9,6 @@ data class AppInfo(
     val backgroundColor: Int,
     val label: String
 ) {
-    val lowerLabel = label.toLowerCase()
+    val lowerLabel = label.toLowerCase(Locale.ROOT)
     val labelComponents = lowerLabel.split(' ')
 }

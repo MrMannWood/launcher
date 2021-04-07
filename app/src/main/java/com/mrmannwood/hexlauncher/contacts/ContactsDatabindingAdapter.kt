95bdf9b843b6ae9d1aa2f3709e84ec6364150d24
Marshall Mann-Wood <marshallmannwood@gmail.com>
2021-04-07 09:36:48 -0700

    added fake contact image for contacts without an image

--- app/src/main/java/com/mrmannwood/hexlauncher/contacts/ContactsDatabindingAdapter.kt
+++ app/src/main/java/com/mrmannwood/hexlauncher/contacts/ContactsDatabindingAdapter.kt
@@ -1,12 +1,33 @@
 package com.mrmannwood.hexlauncher.contacts
 
 import android.content.res.Resources
+import android.graphics.drawable.ColorDrawable
+import android.view.View
+import androidx.core.content.res.ResourcesCompat
 import com.mrmannwood.launcher.R
 
 class ContactsDatabindingAdapter (
     private val resources: Resources
 ) {
 
+    private val blackDrawable by lazy {
+        ColorDrawable(ResourcesCompat.getColor(resources, R.color.black, null))
+    }
+
     fun getContactPictureContentDescription(contact: ContactData) =
         resources.getString(R.string.contact_image_content_description, contact.name)
+
+    fun getContactImageSource(contact: ContactData) = contact.image ?: blackDrawable
+
+    fun getContactLetterVisibility(contact: ContactData) = if (contact.image == null) View.VISIBLE else View.GONE
+
+    fun getContactLetter(contact: ContactData) =
+            contact.name
+                    .split(' ')
+                    .map { it[0] }
+                    .joinToString(
+                            separator = "",
+                            limit = 4,
+                            truncated = ""
+                    ) { it.toString() }
 }

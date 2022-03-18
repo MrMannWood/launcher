2840a8c248d400f84d74155c5ab10d0c3ba5c107
Marshall Mann-Wood <marshallmannwood@gmail.com>
2022-03-17 22:10:28 -0700

    started working on screengrab. It should work but I'm getting instrumentation errors when I run it and don't know why

--- app/src/androidTest/kotlin/com/mrmannwood/hexlauncher/screenshots/TakeScreenshotsTest.kt
+++ app/src/androidTest/kotlin/com/mrmannwood/hexlauncher/screenshots/TakeScreenshotsTest.kt
@@ -5,28 +5,37 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.mrmannwood.hexlauncher.launcher.LauncherActivity
-import com.mrmannwood.launcher.R
-import androidx.test.espresso.contrib.RecyclerViewActions;
 import com.mrmannwood.hexlauncher.settings.PreferenceKeys
 import com.mrmannwood.hexlauncher.settings.PreferencesRepository
 import com.mrmannwood.launcher.BuildConfig
+import com.mrmannwood.launcher.R
 import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
-import tools.fastlane.screengrab.Screengrab
 import org.junit.runner.RunWith
-import java.lang.StringBuilder
+import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy
+
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class TakeScreenshotsTest {
 
     @get:Rule val launcherRule = ActivityScenarioRule(LauncherActivity::class.java)
+
+    companion object {
+        @BeforeClass
+        fun setupClass() {
+            Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
+        }
+    }
 
     @Before
     fun setup() {
